f73d7193112cdffe4984a52607ad1595a6846105
Rickard Magnusson <me@magnusson.dev>
2024-04-20 14:17:24 +0200

    Remove accidently added text

--- src/lib/components/GameOver.svelte
+++ src/lib/components/GameOver.svelte
@@ -12,7 +12,7 @@
 	<img src={currentTrack.cover} alt="" class="mb-2 w-40 md:w-60" />
 	<p>{currentTrack.artist} - <strong>{currentTrack.title}</strong></p>
 	{#if currentTrack.release_date}
-		<p class="text-sm">{currentTrack.release_date}abc</p>
+		<p class="text-sm">{currentTrack.release_date}</p>
 	{/if}
 	<button
 		class="mt-4 block rounded-md bg-purple-700 px-3 py-2 text-sm font-semibold uppercase leading-6 text-neutral-200"
